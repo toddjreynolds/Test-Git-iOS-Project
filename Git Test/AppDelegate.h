fc85020e892e3afd451caba3e8dbae074f32aba6
toddjreynolds <toddjreynolds@toddreynoldss-MacBook-Pro.local>
2014-11-14 12:19:13 -0700

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Todd Reynolds on 11/14/14.
+//  Copyright (c) 2014 toddjreynolds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
